c99154315a83203629e422dd461ca03c6f687a68
Willian Soares <contact@williansoaresdev.com>
2021-11-25 15:00:18 -0300

    Testing the class declaration

--- willregex.js
+++ willregex.js
@@ -1,3 +1,17 @@
 /*
   Documentation for will reg ex lib
 */
+
+function WillRegEx() {
+
+    this.classVersion = function() {
+        return '1.00';
+    }
+
+    /* --- create routine --- */
+    console.log('WillRegEx ' + this.classVersion() + ' was created.');
+
+}
+
+/* creates a new WillRegEx object */
+const willRegEx = new WillRegEx();
