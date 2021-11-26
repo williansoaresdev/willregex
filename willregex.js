05ecb0d82e547b1b2be4bc7105d7645645223583
Willian Soares <contact@williansoaresdev.com>
2021-11-26 09:53:57 -0300

    Testing the function call

--- willregex.js
+++ willregex.js
@@ -4,8 +4,29 @@
 
 function WillRegEx() {
 
+    /* Some necessary expressions */
+    this.constants = {
+        NO_PROCCESS = 'Nothing processed'
+    }
+
+    /* Store the last error found */
+    this.lastError = '';
+
+    /* Control the class version by coding */
     this.classVersion = function() {
         return '1.00';
+    }
+
+    /* Returns the last error found */
+    this.getLastError = function() {
+        return this.lastError;
+    }
+
+    /* ONLY NUMBERS regexp validation */
+    this.onlyNumbers = function(content) {
+        this.lastError = this.constants.NO_PROCCESS;
+
+        return false;
     }
 
     /* --- create routine --- */
